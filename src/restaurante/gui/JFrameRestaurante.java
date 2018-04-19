4574339d19f9458abb7c983d4b1b05c225f48949
iMrJulian <juliandrincon@gmail.com>
2018-04-19 15:55:49 -0500

    fdsfsdf

--- src/restaurante/gui/JFrameRestaurante.java
+++ src/restaurante/gui/JFrameRestaurante.java
@@ -21,6 +21,7 @@ public class JFrameRestaurante extends javax.swing.JFrame {
     public JFrameRestaurante() {
         initComponents();
         ap = new JFrameAgregar();
+        tipo = new Plato();
     }
 
     /**
@@ -88,7 +89,7 @@ public class JFrameRestaurante extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        ap.setVisible(true);
+        this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
